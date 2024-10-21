66efc9e4c3e6bf781fecaafdd29c057de9cf73ec
Abdirahim Ali <abdichange@gmail.com>
2024-10-21 13:31:59 +0300

    rdesign

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,10 +1,9 @@
 document.getElementById("notifybtn").addEventListener("click", function(){
     let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     let email = document.getElementById('email').value;
-    let erroremail = document.getElementById('error-email');
+    let erroremail = document.getElementById('email-error');
     if(email.match(validRegex)) {
         erroremail.textContent = "successfuly registered"
-        return true;
 
     } else {
         erroremail.textContent = "Please provide a valid email address";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.html",
+    "./index.html",
+    "./assets/js/validation.js",
   ],
   theme: {
     fontFamily : {
-      Libra : ['Libre Franklin', 'sans'],    },
+      Libra : ['Libre Franklin', 'sans'],
+     },
+     colors : {
+      Black : '#000',
+      White : '#FFF',
+      Gray : '#CAD3DB',
+      Gray2 : '#969696',
+      Blue : '#4C7BF3',
+      Black2 : '#15202A',
+      BLue2 : '#C2D3FF',
+      Blue3 : '#B8C7ED',
+      Red3 : '#FF5466',
+    },
+    fontSize : {
+      'preset-1' : ['48px', {
+        lineHeight : '60px',
+        letterSpacing : '0x',
+        fontWeight : '400',
+      }],
+      'preset-2' : ['20px', {
+        lineHeight : '24px',
+        letterSpacing : '0x',
+        fontWeight : '400',
+      }],
+      'preset-3' : ['16px', {
+        lineHeight : '19px',
+        letterSpacing : '0x',
+        fontWeight : '400',
+      }],
+      'preset-4' : ['12px', {
+        lineHeight : '15px',
+        letterSpacing : '0x',
+        fontWeight : '400',
+      }],
+      'preset-5' : ['22px', {
+        lineHeight : '32px',
+        letterSpacing : '0x',
+        fontWeight : '400',
+      }],
+    },
     extend: {
-      colors : {
-        Black : '#000',
-        White : '#FFF',
-        Gray : '#CAD3DB',
-        Gray2 : '#969696',
-        Blue : '#4C7BF3',
-        Black2 : '#15202A',
-        BLue2 : '#C2D3FF',
-        Blue3 : '#B8C7ED',
-        Red3 : '#FF5466',
-      },
-      fontSize : {
-        'preset-1' : ['48px', {
-          lineHeight : '60px',
-          letterSpacing : '0x',
-          fontWeight : '400',
-        }],
-        'preset-2' : ['20px', {
-          lineHeight : '24px',
-          letterSpacing : '0x',
-          fontWeight : '400',
-        }],
-        'preset-3' : ['16px', {
-          lineHeight : '19px',
-          letterSpacing : '0x',
-          fontWeight : '400',
-        }],
-        'preset-4' : ['12px', {
-          lineHeight : '15px',
-          letterSpacing : '0x',
-          fontWeight : '400',
-        }],
-      },
-      spacing : {
-        6 : '6px',
-        // 20 : '20px',
-        25 : '25px',
-        28 : '28px',
-        31 : '31px',
-        49 : '49px',
-        56 : '56px',
-        94 : '94px',
-        72 : '72px',
-        200 : '200px',
-        320 : '320px',
-        383 : '383px',
-        421 : '421px',
-        712 : '712px',
-        640 : '640px',
-      },
       borderRadius : {
         28 : '28px',
+      },
+      boxShadow : {
+        buttonshadow :'0px 5px 10px 2px rgba(76, 123, 243, 0.2304)',
       }
     },
   },
